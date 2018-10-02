4a2f8ae14f5972806ade5796d42b6b8aae2e135e
Dieter Flick <dflick@pivotal.io>
2018-10-02 11:50:52 +0200

    first commit

--- fortune-teller-fortune-service/src/main/resources/import.sql
+++ fortune-teller-fortune-service/src/main/resources/import.sql
@@ -4,7 +4,7 @@ insert into fortunes (id, text) values (1002, 'The time is right to make new fri
 insert into fortunes (id, text) values (1003, 'A thrilling time is in your immediate future. ');
 insert into fortunes (id, text) values (1004, 'Something you lost will soon turn up. ');
 insert into fortunes (id, text) values (1005, 'Your many hidden talents will become obvious to those around you. ');
-insert into fortunes (id, text) values (1006, 'A member of your family will soon do something that will make you proud.Happy news is on its way to you. ');
+insert into fortunes (id, text) values (1006, 'A member of your family will soon do something that will make you proud. ');
 insert into fortunes (id, text) values (1007, 'You will have a very pleasant experience. ');
 insert into fortunes (id, text) values (1008, 'You will live a long, happy life. ');
 insert into fortunes (id, text) values (1009, 'You will take a chance in something in the near future. ');
