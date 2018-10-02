39797bb53931c44eca212d350944fcc0bdbc9e5d
Dieter Flick <dflick@pivotal.io>
2018-10-02 11:36:55 +0200

    first commit

--- scripts/pws-create-services.sh
+++ scripts/pws-create-services.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cf create-service cleardb spark db
-cf create-service p-config-server trial config-server -c '{"git": { "uri": "https://github.com/spring-cloud-services-samples/fortune-teller", "searchPaths": "configuration" } }'
+cf create-service p-config-server trial config-server -c '{"git": { "uri": "https://github.com/dflick-pivotal/summit-teller", "searchPaths": "configuration" } }'
 cf create-service p-service-registry trial service-registry
 cf create-service p-circuit-breaker-dashboard trial circuit-breaker
